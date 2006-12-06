3c1e3ca7ff4420301e2453f1bb8af91c83e50866
retrakker <retrakker@d65a7696-613b-0410-ac10-aa9ec39ffaa2>
2006-12-06 02:14:32 +0000

    - added reading of environment variable ARTOOLKIT_CONFIG for DSVL video capture

--- artoolkit/lib/SRC/VideoWin32DirectShow/videoWin32DirectShow.cpp
+++ artoolkit/lib/SRC/VideoWin32DirectShow/videoWin32DirectShow.cpp
@@ -155,7 +155,14 @@ AR2VideoParamT *ar2VideoOpen(char *config)
 	
 	vid->graphManager = new DSVL_VideoSource();
 	if (!config) {
-		if (FAILED(vid->graphManager->BuildGraphFromXMLString(config_default))) return(NULL);
+
+		config = getenv("ARTOOLKIT_CONFIG");
+
+		if (config == NULL) {
+			config = &config_default[0];
+		}
+		if (FAILED(vid->graphManager->BuildGraphFromXMLString(config))) return(NULL);
+
 	} else {
 		if (strncmp(config, "<?xml", 5) == 0) {
 			if (FAILED(vid->graphManager->BuildGraphFromXMLString(config))) return(NULL);
